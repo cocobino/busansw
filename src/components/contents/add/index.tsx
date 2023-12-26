987ecfe6a2301bf27274feddf6999a9df801a868
cocobino <coolbino@naver.com>
2023-12-27 08:29:14 +0900

    - 스타일 수정

--- src/components/contents/add/index.tsx
+++ src/components/contents/add/index.tsx
@@ -4,6 +4,11 @@ const ContentsAdd = () => {
 
 
     return <div className={css`
+      width: 100%;
+      height: 50px;
+      display: flex;
+      align-content: center;
+      justify-content: center;
     `}>+</div>
 }
 
